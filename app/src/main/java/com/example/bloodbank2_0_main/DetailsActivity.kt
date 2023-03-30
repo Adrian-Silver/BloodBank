22dd0c26b5eb06b31a65634d52540e6fa8676cb1
Adrian-Silver <adrianallangitau@gmail.com>
2023-03-30 12:15:51 +0300

    DetailsActivity edit

--- app/src/main/java/com/example/bloodbank2_0_main/DetailsActivity.kt
+++ app/src/main/java/com/example/bloodbank2_0_main/DetailsActivity.kt
@@ -2,10 +2,18 @@ package com.example.bloodbank2_0_main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.bloodbank2_0_main.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
